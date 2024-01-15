9a333886f92404314f084800c1f59aa92260e1f1
Joakim <joakim@users.noreply.github.com>
2024-01-15 10:30:01 +0100

    Add test case for Compel's example
    A nod to Horace Enea and Larry Tesler

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -14,11 +14,26 @@ test('interface', () => {
 
 test('basic', async () => {
   const dataflow = new Dataflow({ out: (x: number, y: number) => x + y }, debug)
-  expect(dataflow.values).toStrictEqual({})
 
   await dataflow.set({ x: 1, y: 2 })
   expect(dataflow.values).toStrictEqual({ x: 1, y: 2, out: 3 })
 
   const out = await dataflow.get('out')
   expect(out).toStrictEqual(3)
+})
+
+test('compel', async () => {
+  const compel = new Dataflow(
+    {
+      out: (a, e, d) => (a - e) / d,
+      e: (a, b, c) => a * b - c,
+      d: (a, b) => a - b,
+    },
+    debug
+  )
+
+  await compel.set({ a: 6, b: 7, c: 8 })
+
+  const out = await compel.get('out')
+  expect(out).toStrictEqual(28)
 })
